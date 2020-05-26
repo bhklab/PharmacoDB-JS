61ef8519d1aee755d97553beb52de212af8b21b4
gangeshberi <gangeshberi@gmail.com>
2020-05-26 12:16:16 -0400

    Initializing `graphql` and setting up the folder structure.

--- server/app.js
+++ server/app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
 
+// importing the graphql schema and resolver functions.
+const graphQlSchema = require('./graphql/schema/index');
+const graphQlResolvers = require('./graphql/resolvers/index');
+
+// express server.
 const app = express();
 
+// body parser.
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Basic Setup!');
-})
+
+// setting up the graphql end points.
+// passing in the graphql schema and resolver functions.
+app.use('/graphql', graphqlHttp({
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
+    graphiql: true
+}));
+
+
 
 app.listen(5000);
