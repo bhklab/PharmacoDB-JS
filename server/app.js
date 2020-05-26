fe0e11a3cfd0c0af5c5448fa6234817a6fd092a8
gangeshberi <gangeshberi@gmail.com>
2020-05-26 11:43:49 -0400

    `Initial Commit`: Setting up the node/express server.

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res, next) => {
+    res.send('Basic Setup!');
+})
+
+app.listen(5000);
