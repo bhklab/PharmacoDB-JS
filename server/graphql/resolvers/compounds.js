e714b093e3361935af3b2aa6aa1964d796401354
gangeshberi <gangeshberi@gmail.com>
2020-05-27 15:31:30 -0400

    updating `compound` resolver in order to return the correct result.

--- server/graphql/resolvers/compounds.js
+++ server/graphql/resolvers/compounds.js
@@ -4,7 +4,12 @@ module.exports = {
     compounds: async () => {
         try {
             const compounds = await knex.select().from('drugs');
-            console.log(compounds)
+            return compounds.map((compound) => {
+                return {
+                    id: compound.drug_id,
+                    name: compound.drug_name
+                }
+            })
         } catch (err) {
             throw err;
         }
