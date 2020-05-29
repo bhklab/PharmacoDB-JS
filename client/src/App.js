35d980832de2f78417cf7a62e5edf79c8b86a888
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2020-05-29 17:27:27 -0400

    setting up the apollo graphql client.

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+
 import {
   Drugs
 } from './Components/index';
 
 
+// apollo client setup.
+const client =  new ApolloClient({
+  uri: 'http://localhost:5000/graphql' // making requests to this endpoint.
+})
+
+
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Drugs} />
-      </Switch>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Drugs} />
+        </Switch>
+      </Router>
+    </ApolloProvider>
   );
 }
 
