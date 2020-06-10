53cac7a08c56bc179f37154877f0336189c9af74
gangeshberi <gangeshberi@gmail.com>
2020-06-10 16:59:10 -0400

    changing format for migration files!

--- server/db/migrations/20200610165155_create_compound_table.js
+++ server/db/migrations/20200610165155_create_compound_table.js
@@ -1,4 +1,4 @@
-exports.up = function(knex, Promise) {
+exports.up = function(knex) {
     return knex.schema
         .hasTable('compounds')
         .then(exists => {
@@ -14,6 +14,6 @@ exports.up = function(knex, Promise) {
         });
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function(knex) {
     return knex.schema.dropTableIfExists('compounds');
 };
