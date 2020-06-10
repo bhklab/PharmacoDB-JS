2cbc34ef388a7160d7be6b9f945511aa6faad822
gberi <gangeshberi1@gmail.com>
2020-06-10 12:26:02 -0400

    prettier and eslint changes!

--- server/db/migrations/20200527121511_drugs.js
+++ server/db/migrations/20200527121511_drugs.js
@@ -1,19 +1,17 @@
-
 exports.up = function(knex, Promise) {
-    return knex.schema.hasTable('compounds')
-        .then((exists) => {
-            if(!exists) {
-                return knex.schema.createTable('compounds', (table) => {
-                    table.increments('id')
-                        .primary();
-                    table.string('name')
-                        .notNullable();
-                })
+    return knex.schema
+        .hasTable('compounds')
+        .then(exists => {
+            if (!exists) {
+                return knex.schema.createTable('compounds', table => {
+                    table.increments('id').primary();
+                    table.string('name').notNullable();
+                });
             }
         })
         .catch(err => {
             throw err;
-        })
+        });
 };
 
 exports.down = function(knex, Promise) {
