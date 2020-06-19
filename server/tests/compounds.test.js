4abde3f15930afe0b19a29203e25a6a113523f50
Minoru Nakano <minoru.nakano@gmail.com>
2020-06-19 10:44:51 -0400

    Modified the test file to use an app instance for testing.

--- server/tests/compounds.test.js
+++ server/tests/compounds.test.js
@@ -1,10 +1,18 @@
+const app = require('../app'); // import NodeJS app
 const chai = require('chai');
 
 const expect = chai.expect;
-const url = 'http://localhost:5000';
-const request = require('supertest')(url);
 
-describe('Compounds GraphQL API', () => {
+// Load supertest object with app.
+const request = require('supertest')(app);
+
+describe('Test: Compounds API', () => {
+    
+    // Terminate the server after all the tests are done.
+    after(() => {
+        process.exit(0)
+    })
+
     it('Returns "id" and "name" properties of all compounds in the database', (done) => {
         request.post('/graphql')
         .send({query: '{ compounds { id name } }'})
