ab23ccc9425451b264abd19de9df4d7b3df12d23
gangeshberi <gangeshberi@gmail.com>
2020-06-19 15:31:09 -0400

    comments in the root query.

--- server/graphql/schema/root_query.js
+++ server/graphql/schema/root_query.js
@@ -1,10 +1,19 @@
 // root query for the schema definition.
 const RootQuery = `type RootQuery {
+    "Root Queries for compounds."
     compounds: [Compound!]!
     compound(compoundId: Int!): Compound!
+
+    "Root Queries for cell lines."
     cell_lines: [CellLine!]!
+
+    "Root Queries for datasets."
     datasets: [Dataset!]!
+
+    "Root Queries for tissues."
     tissues: [Tissue!]!
+
+    "Root Queries for genes."
     genes: [Gene!]!
     gene(geneId: Int!): Gene!
 }`;
