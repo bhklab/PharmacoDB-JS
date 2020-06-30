ee497be74b401e8156daa459e3cfe7d810e5457c
gangeshberi <gangeshberi@gmail.com>
2020-06-30 15:21:49 -0400

    adding additional function to dataset API and new type.

--- server/graphql/resolvers/dataset.js
+++ server/graphql/resolvers/dataset.js
@@ -6,12 +6,27 @@ const knex = require('../../db/knex');
 const datasets = async () => {
     try {
         const datasets = await knex.select().from('datasets');
+        const cell_count = await knex
+            .select('source_name')
+            .countDistinct('cell_id as cell_count')
+            .from('source_cell_names as sc')
+            .join('sources as s', 's.source_id', 'sc.source_id')
+            .groupBy('sc.source_id');
+
         return datasets.map(dataset => {
             return {
                 id: dataset.dataset_id,
                 name: dataset.dataset_name
             };
         });
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
+};
+
+const dataset = async args => {
+    try {
     } catch (err) {
         console.log(err);
         throw err;
--- server/graphql/schema/dataset.js
+++ server/graphql/schema/dataset.js
@@ -2,6 +2,9 @@ const datasetType = `
     type Dataset {
         id: Int!
         name: String!
+        tested_cells: Int!
+        tested_tissues: Int!
+        tested_compounds: Int!
     }
 `;
 
