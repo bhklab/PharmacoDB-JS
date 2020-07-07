66f339648f2e45848813152ef4f22ec8f00f539f
chuntul <chantal.ho98@hotmail.com>
2020-07-07 14:18:56 -0400

    sidebar scrolling highlight + temp divs

--- client/src/Components/IndivCompounds/IndivCompounds.js
+++ client/src/Components/IndivCompounds/IndivCompounds.js
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { useQuery } from '@apollo/react-hooks';
-import { Link } from 'react-router-dom';
+import { Link, Element, animateScroll as scroll } from 'react-scroll';
 import PropTypes from 'prop-types';
 import Layout from '../Layout/Layout';
 import { getCompoundQuery } from '../../queries/queries';
@@ -17,15 +17,41 @@ const StyledIndivCompounds = styled.div`
     font-family: 'Roboto Slab', serif;
     font-size: calc(2vw + 1.5em);
   }
+  .container {
+    width: 100%;
+    margin-top: 5vh;
+    
+    display:flex;
+    align-items: flex-end;
+    flex-direction: column;
+    
+    .content{
+      width: 75%;
+    }
+  }
+  .temp {
+    width: 100%;
+    height: 400px;
+    background: ${colors.light_blue_bg};
+    margin: 20px 0px;
+  }
+
+  // full size container when too small
+  @media only screen and (max-width: 765px) {
+    .content {
+      width: 100% !important;
+    }
+  }
 
 `;
 
 const StyledSidebar = styled.div`
-  width: calc(5vw + 5em);
+  width: calc(5vw + 4em);
   margin-top: 5vh;
   padding: 5px 0px;
+  position:fixed;
 
-  a {
+  .link {
     display:block;
     color: ${colors.dark_teal_heading};
     border-right: 5px solid ${colors.light_blue_header};
@@ -33,13 +59,22 @@ const StyledSidebar = styled.div`
     font-family: 'Overpass', sans-serif;
     text-align: right;
     padding:20px 20px 20px 0px;
+    transition: all 0.25s ease-out 0s;
+    cursor: pointer;
   }
-  a:hover {
+  .link:hover {
+    color: ${colors.dark_pink_highlight};
+    border-right: 5px solid ${colors.dark_pink_highlight};
+    transition: all 0.25s ease-out 0s;
+  }
+  .selected {
     color: ${colors.dark_pink_highlight};
     border-right: 5px solid ${colors.dark_pink_highlight};
   }
-  .active {
-    border-right: 5px solid ${colors.dark_pink_highlight};
+
+  // hide sidebar when too small
+  @media only screen and (max-width: 765px) {
+    display:none;
   }
 
 `;
@@ -78,11 +113,18 @@ const IndivCompounds = (props) => {
               <StyledIndivCompounds>
                 <h1>{compound.name}</h1>
                 <StyledSidebar>
-                  <Link to="#synonyms">Synonyms</Link>
-                  <Link to="#external_ids">External IDs</Link>
-                  <Link to="#plots">Plots</Link>
+                  <Link className="link" activeClass="selected" to="synonyms" spy smooth duration={200} offset={-400}>Synonyms</Link>
+                  <Link className="link" activeClass="selected" to="external_ids" spy smooth duration={200} offset={-400}>External IDs</Link>
+                  <Link className="link" activeClass="selected" to="plots" spy smooth duration={200} offset={-400}>Plots</Link>
                 </StyledSidebar>
-                <div id="synonyms" />
+                <div className="container">
+                  <div className="content">
+                    <Element name="synonyms" className="temp">Synonyms</Element>
+                    <Element name="external_ids" className="temp">External ids</Element>
+                    <Element name="plots" className="temp">plots</Element>
+                  </div>
+
+                </div>
               </StyledIndivCompounds>
             ))}
       </StyledWrapper>
