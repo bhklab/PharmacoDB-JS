350ef9fdfe2d6f80335f1ecaa6ded20efc3407b1
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2020-07-10 18:16:58 -0400

    added a new rule to eslint and comment type changed from using `#` to `"`.

--- graphql/schema/dataset.js
+++ graphql/schema/dataset.js
@@ -1,25 +1,29 @@
 const datasetType = `
     type Dataset {
+        "id of the dataset in the database"
         id: Int!
+        "name of the dataset"
         name: String!  
     }
 `;
 
 const datasetInformationType = `
     type DatasetInformation {
+        "id of the dataset"
         id: Int!
+        "name of the dataset"
         name: String!
-        # number of cell-lines in the dataset.
+        "number of cell-lines in the dataset"
         cells: Int!
-        # number of tissues in the dataset.
+        "number of tissues in the dataset"
         tissues: Int!
-        # number of compounds in the dataset.
+        "number of compounds in the dataset"
         compounds: Int!
-        # number of experiments held across the dataset.
+        "number of experiments held across the dataset"
         experiments: Int!
-        # cells tested in the dataset.
+        "cell lines tested in the dataset"
         cells_tested: [String!]
-        # compounds tested in the dataset.
+        "compounds tested in the dataset"
         compounds_tested: [String!]
     }
 `;
