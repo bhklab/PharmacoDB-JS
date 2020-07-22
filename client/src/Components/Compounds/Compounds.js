f88e1f620f47c1c2a36a6074bbfeb42ebd0e393d
chuntul <chantal.ho98@hotmail.com>
2020-07-22 17:22:11 -0400

    minor styling with a wrapper on the compounds page

--- client/src/Components/Compounds/Compounds.js
+++ client/src/Components/Compounds/Compounds.js
@@ -4,6 +4,8 @@ import { useQuery } from '@apollo/react-hooks';
 import Layout from '../Utils/Layout';
 import { getCompoundsQuery } from '../../queries/queries';
 
+import StyledWrapper from '../../styles/utils';
+
 /**
  * Parent component for the compounds page.
  *
@@ -19,18 +21,20 @@ const Compounds = () => {
 
   return (
     <Layout>
-      {loading ? (<p>Loading...</p>)
-        : (error ? (<p>Error!</p>)
-          : data.compounds.map(({ id, name }) => (
-            <div key={id}>
-              <p>
-                {id}
-                :
-                {' '}
-                {name}
-              </p>
-            </div>
-          )))}
+      <StyledWrapper>
+        {loading ? (<p>Loading...</p>)
+          : (error ? (<p>Error!</p>)
+            : data.compounds.map(({ id, name }) => (
+              <div key={id}>
+                <p>
+                  {id}
+                  :
+                  {' '}
+                  {name}
+                </p>
+              </div>
+            )))}
+      </StyledWrapper>
 
     </Layout>
   );
