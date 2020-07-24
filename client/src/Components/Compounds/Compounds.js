e0433a06ab00d0e9becb754342afb77fa8d93e97
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2020-07-24 14:15:41 -0400

    very basic pagination.

--- client/src/Components/Compounds/Compounds.js
+++ client/src/Components/Compounds/Compounds.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-nested-ternary */
 import React, { useState, useMemo } from 'react';
-import { useTable, useSortBy } from 'react-table'
+import { useTable, useSortBy, usePagination } from 'react-table'
 import { useQuery } from '@apollo/react-hooks';
 import Layout from '../Utils/Layout';
 import { getCompoundsQuery } from '../../queries/queries';
@@ -10,30 +10,34 @@ import styled from 'styled-components';
 const Styles = styled.div`
   padding: 1rem;
 
-  table {
-    border-spacing: 0;
-    border: 1px solid black;
+	table {
+		border-spacing: 0;
+		border: 1px solid black;
 
-    tr {
-      :last-child {
-        td {
-          border-bottom: 0;
-        }
-      }
-    }
+		tr {
+			:last-child {
+				td {
+					border-bottom: 0;
+				}
+			}
+		}
 
-    th,
-    td {
-      margin: 0;
-      padding: 0.5rem;
-      border-bottom: 1px solid black;
-      border-right: 1px solid black;
+		th,
+		td {
+			margin: 0;
+			padding: 0.5rem;
+			border-bottom: 1px solid black;
+			border-right: 1px solid black;
 
-      :last-child {
-        border-right: 0;
-      }
-    }
-  }
+			:last-child {
+				border-right: 0;
+			}
+		}
+	}
+
+	.pagination {
+		padding: 0.5rem;
+	}
 `
 
 
@@ -87,51 +91,110 @@ const Table = ({ columns, data }) => {
 	  headerGroups,
 	  rows,
 	  prepareRow,
+	  page,
+	  canPreviousPage,
+	  canNextPage,
+	  pageOptions,
+	  pageCount,
+	  gotoPage,
+	  nextPage,
+	  previousPage,
+	  setPageSize,
+	  state: { pageIndex, pageSize },
 	} = useTable(
 		{
 			columns,
 			data,
+			initialState: { pageIndex: 0 },
 		},
-		useSortBy
+		useSortBy,
+		usePagination
 	)
   
 	// Render the UI for your table
 	return (
-	  <table {...getTableProps()}>
-		<thead>
-		  {headerGroups.map(headerGroup => (
-			<tr {...headerGroup.getHeaderGroupProps()}>
-			  {headerGroup.headers.map(column => (
-				// Add the sorting props to control sorting. For this example
-                // we can add them into the header props
-				<th {...column.getHeaderProps(column.getSortByToggleProps())}>
-					{column.render('Header')}
-					{/* Add a sort direction indicator */}
-					<span>
-						{column.isSorted
-						? column.isSortedDesc
-							? ' 🔽'
-							: ' 🔼'
-						: ''}
-                  	</span>
-				</th>
-			  ))}
-			</tr>
-		  ))}
-		</thead>
-		<tbody {...getTableBodyProps()}>
-		  {rows.map((row, i) => {
-			prepareRow(row)
-			return (
-			  <tr {...row.getRowProps()}>
-				{row.cells.map(cell => {
-				  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+		<>
+			<table {...getTableProps()}>
+			<thead>
+				{headerGroups.map(headerGroup => (
+				<tr {...headerGroup.getHeaderGroupProps()}>
+					{headerGroup.headers.map(column => (
+					// Add the sorting props to control sorting. For this example
+					// we can add them into the header props
+					<th {...column.getHeaderProps(column.getSortByToggleProps())}>
+						{column.render('Header')}
+						{/* Add a sort direction indicator */}
+						<span>
+							{column.isSorted
+							? column.isSortedDesc
+								? ' 🔽'
+								: ' 🔼'
+							: ''}
+						</span>
+					</th>
+					))}
+				</tr>
+				))}
+			</thead>
+			<tbody {...getTableBodyProps()}>
+				{rows.map((row, i) => {
+				prepareRow(row)
+				return (
+					<tr {...row.getRowProps()}>
+					{row.cells.map(cell => {
+						return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+					})}
+					</tr>
+				)
 				})}
-			  </tr>
-			)
-		  })}
-		</tbody>
-	  </table>
+			</tbody>
+			</table>
+			<div className="pagination">
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>{' '}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>{' '}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>{' '}
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        <span>
+          | Go to page:{' '}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={e => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0
+              gotoPage(page)
+            }}
+            style={{ width: '100px' }}
+          />
+        </span>{' '}
+        <select
+          value={pageSize}
+          onChange={e => {
+            setPageSize(Number(e.target.value))
+          }}
+        >
+          {[10, 20, 30, 40, 50].map(pageSize => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+		</>
+
 	)
   }
 
