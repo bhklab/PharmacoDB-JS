736e33a8be288db8f1945de1fdf25c0bbb71113c
Minoru Nakano <minoru.nakano@gmail.com>
2020-07-28 17:37:08 -0400

    Added test cases for the compound API.

--- tests/graphql/compound.test.js
+++ tests/graphql/compound.test.js
@@ -1,20 +1,25 @@
 /**
- * Unit test code for testing GraphQL Compount API endpoints.
+ * Unit test code for testing GraphQL Compound API endpoints.
  */
 const chai = require('chai');
 const expect = chai.expect;
 const request = require('supertest');
+const queries = require('./queries/compound_queries');
 
 /**
  * This describe block encompasses tests for the Compound API.
  * Individual test cases for each function within the compound is organized under a nested describe block.
  */
 describe('Tests: Compound API', () => {
+    let server = null;
+    let knex = null;
     // declare variables used in this test block.
     before(function(){
         this.failures = [];
         this.successes = [];
-        
+        delete require.cache[require.resolve('../../app')];
+        knex = require('../../db/knex');
+        server = require('../../app');
     })
 
     // Terminate the server after all the tests are done.
@@ -22,11 +27,13 @@ describe('Tests: Compound API', () => {
         if (this.failures.length) {
             console.log('\tGraphQL: The following ' + this.failures.length + ' test(s) failed:');
             this.failures.forEach(f => console.log('\t\t' + f));
-            done();
+            //done();
         } else {
             console.log('\tGraphQL: Passed all ' + this.successes.length + ' test(s).');
-            done();
+            //done();
         }
+        knex.destroy();
+        server.close(done);
     });
 
     /**
@@ -35,14 +42,14 @@ describe('Tests: Compound API', () => {
      * Each describe block should have an "afterEach" hook which resets database connection and server instance, and logs a test result to 
      * "successes" or "failures" array.
      */
-    describe('compounds function', () => {
-        let server = null;
-        let knex = null;
+    describe('Compound API', () => {
+        // let server = null;
+        // let knex = null;
         // load a brandnew server and db instances before each test.
         beforeEach(function(){
-            delete require.cache[require.resolve('../../app')];
-            knex = require('../../db/knex');
-            server = require('../../app');
+            // delete require.cache[require.resolve('../../app')];
+            // knex = require('../../db/knex');
+            // server = require('../../app');
         })
 
         // Log test result after each test.
@@ -53,18 +60,19 @@ describe('Tests: Compound API', () => {
                 this.failures.push(this.currentTest.title);
             }
             // close the server and db instances after each test.
-            knex.destroy();
-            server.close(done);
+            // knex.destroy();
+            // server.close(done);
+            done();
         });
     
         /**
-         * An example test case for the Compound API.
-         * Each test case should have a title that describes an expected outcome.
+         * compounds query
+         * Returns "id" and "name" properties of all compounds in the database
          */
         it('Returns "id" and "name" properties of all compounds in the database', done => {
             request(server)
                 .post('/graphql')
-                .send({ query: '{ compounds { id name } }' })
+                .send({ query: queries.compoundsKeysTestQuery })
                 .expect(200)
                 .end((err, res) => {
                     if (err) return done(err);
@@ -74,6 +82,43 @@ describe('Tests: Compound API', () => {
                     return done();
                 });
         });
+
+        /**
+         * compound query
+         * Returns a compound object based on a compound ID.
+         */
+        it('Returns a compound object based on a compound ID input.', done => {
+            request(server)
+                .post('/graphql')
+                .send({ query: queries.compoundKeysTestQuery })
+                .expect(200)
+                .end((err, res) => {
+                    if (err) return done(err);
+                    const compound = res.body.data.compound.compound;
+                    expect(compound).to.have.keys('id', 'name', 'annotation');
+                    expect(compound.annotation).to.have.keys('smiles', 'inchikey', 'pubchem');
+                    return done();
+                });
+        });
+
+        /**
+         * compound query
+         * Returns a compound object for paclitaxel.
+         */
+        it('Returns a compound object for paclitaxel.', done => {
+            request(server)
+                .post('/graphql')
+                .send({ query: queries.compoundQueryPaclitaxel })
+                .expect(200)
+                .end((err, res) => {
+                    if (err) return done(err);
+                    const compound = res.body.data.compound.compound;
+                    expect(compound.id).to.equal(526);
+                    expect(compound.name).to.equal('paclitaxel');
+                    return done();
+                });
+        });
+
     });
 })
 
---
+++ tests/graphql/queries/compound_queries.js
@@ -0,0 +1,44 @@
+/**
+ * Queries to be used for compound.test.js
+ */
+
+const compoundsKeysTestQuery = `{ 
+    compounds { 
+        id 
+        name 
+    } 
+}`
+
+const compoundKeysTestQuery = `{ 
+    compound(compoundId: 1) {
+        compound {
+            id 
+            name 
+            annotation {
+                smiles
+                inchikey
+                pubchem
+            }
+        } 
+    } 
+}`
+
+const compoundQueryPaclitaxel = `{
+    compound(compoundId: 526) {
+        compound {
+            id 
+            name 
+            annotation {
+                smiles
+                inchikey
+                pubchem
+            }
+        } 
+    }
+}`
+
+module.exports = {
+    compoundsKeysTestQuery,
+    compoundKeysTestQuery,
+    compoundQueryPaclitaxel
+}
