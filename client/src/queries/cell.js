795caa2b46f70e237cb8010c7eb3f5d76f620403
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2020-08-05 18:02:56 -0400

    cell line queries and documentation.

---
+++ client/src/queries/cell.js
@@ -0,0 +1,45 @@
+import { gql } from 'apollo-boost';
+
+/**
+ * @returns - Query returns the list of cell lines with it's id and name,
+ * and also returns the tissue object for the particular cell line.
+ */
+const getCellLinesQuery = gql`
+  query getAllCellLines {
+    cell_lines {
+      id
+      name
+    }
+    tissue {
+      id
+      name
+    }
+  }
+`;
+
+/**
+ * @param { Number } cellId - cell id of the cell line to be queried.
+ * @returns - all the information returns by the cell lines query
+ * and synonym for the cell line in different datasets.
+ */
+const getCellLineQuery = gql`
+  query getSingleCellLine($cellId: Int!) {
+    cell_line(cellId: $cellId) {
+      id
+      name
+      tissue {
+        id
+        name
+      }
+      synonyms {
+        name
+        source
+      }
+    }
+  }
+`;
+
+export {
+  getCellLinesQuery,
+  getCellLineQuery,
+};
