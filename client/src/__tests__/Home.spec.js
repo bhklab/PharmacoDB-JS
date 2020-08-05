e465646df77577c1692448393df82f5ddb225568
chuntul <chantal.ho98@hotmail.com>
2020-08-04 20:10:02 -0400

    fixed tests to be compliant with the search bar coming in with data by wrapping things in async act(), updating snapshots, fixing testing errors created by plotly, updating queries to have the right fields

--- client/src/__tests__/Home.spec.js
+++ client/src/__tests__/Home.spec.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 // wrap in browserrouter or it produces an error that
 // says you can't use link outside a router
 import { BrowserRouter } from 'react-router-dom';
 import Home from '../Components/Home/Home';
 
-// Catch-all snapshot test for entire rendering of home
+// Catch-all snapshot test for shallow rendering of home
+// I chose to do shallow rendering because mounting would mean I'd have to
+// worry about the async act for mounting the search bar.
 it('renders Home correctly', () => {
-  const component = mount(
+  const component = shallow(
     <BrowserRouter>
       <Home />
     </BrowserRouter>,
