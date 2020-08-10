c10c22354695c9866374352b065966c2c2f2a362
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2020-08-10 14:31:36 -0400

    source stats API testing with the right query.

--- tests/graphql/queries/source_queries.js
+++ tests/graphql/queries/source_queries.js
@@ -15,6 +15,21 @@ const allSourcesTestQuery = `
   }
 `;
 
+const sourceStatsTestQuery = `
+  query getSourceStats {
+    source_stats {
+      source_id
+      source_name
+      cell_line_count
+      tissue_count
+      compound_count
+      experiment_count
+    }
+  }
+`;
+
+
 module.exports = {
-    allSourcesTestQuery
+    allSourcesTestQuery,
+    sourceStatsTestQuery
 };
--- tests/graphql/tests/source_test.js
+++ tests/graphql/tests/source_test.js
@@ -25,16 +25,40 @@ const test = (server) => {
                     // expect to have all the data for source object.
                     expect(source).to.have.all.keys('id', 'name', 'dataset');
                     // expect id to be a number.
-                    expect(id).to.be.a('number'),
+                    expect(id).to.be.a('number');
                     // expect name to be a string.
                     expect(name).to.be.a('string');
 
                     // expect id and name for dataset object.
                     expect(dataset).to.have.all.keys('id', 'name');
                     // expect id to be a number.
-                    expect(dataset.id).to.be.a('number'),
+                    expect(dataset.id).to.be.a('number');
                     // expect name to be a string.
                     expect(dataset.name).to.be.a('string');
+                });
+                return done();
+            });
+    });
+
+    it('Test to validate the source stats', done => {
+        request(server)
+            .post('/graphql')
+            .send({query: sourceQueries.sourceStatsTestQuery})
+            .expect(200)
+            .end((err, res) => {
+                if (err) return done(err);
+                const { source_stats } = res.body.data;
+                source_stats.every(stat => {
+                    const { source_id, source_name, cell_line_count, tissue_count, compound_count, experiment_count } = stat;
+                    // testing to see if stat object has all the keys.
+                    expect(stat).to.have.all.keys('source_id', 'source_name', 'cell_line_count', 'tissue_count', 'compound_count', 'experiment_count');
+                    // check for the data type.
+                    expect(source_id).to.be.a('number');
+                    expect(source_name).to.be.a('string');
+                    expect(cell_line_count).to.be.a('number');
+                    expect(tissue_count).to.be.a('number');
+                    expect(compound_count).to.be.a('number');
+                    expect(experiment_count).to.be.a('number');
                 });
                 return done();
             });
