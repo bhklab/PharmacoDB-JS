6b737b5553b051d91c56c35ef0109a675a93037c
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2020-08-10 14:32:31 -0400

    commented out the required code, so uncommenting it.

--- tests/graphql/graphql.test.js
+++ tests/graphql/graphql.test.js
@@ -27,30 +27,30 @@ describe('Tests: GraphQL API', () => {
         await knex.destroy();
     });
 
-    // // tests for Compound API Routes
-    // describe('Compound API Routes', () => {
-    //     require('./tests/compound_test').test(server);
-    // });
+    // tests for Compound API Routes
+    describe('Compound API Routes', () => {
+        require('./tests/compound_test').test(server);
+    });
 
-    // // tests for Cell API Routes
-    // describe('Cell API Routes', () => {
-    //     require('./tests/cell_test').test(server);
-    // });
+    // tests for Cell API Routes
+    describe('Cell API Routes', () => {
+        require('./tests/cell_test').test(server);
+    });
 
-    // // tests for Dataset API routes
-    // describe('Dataset API Routes', () => {
-    //     require('./tests/dataset_test').test(server);
-    // });
+    // tests for Dataset API routes
+    describe('Dataset API Routes', () => {
+        require('./tests/dataset_test').test(server);
+    });
 
-    // // tests for Gene API Routes
-    // describe('Gene API Routes', () => {
-    //     require('./tests/gene_test').test(server);
-    // });
+    // tests for Gene API Routes
+    describe('Gene API Routes', () => {
+        require('./tests/gene_test').test(server);
+    });
 
-    // // tests for Experiment API routes
-    // describe('Experiment API Routes', () => {
-    //     require('./tests/experiment_test').test(server);
-    // });
+    // tests for Experiment API routes
+    describe('Experiment API Routes', () => {
+        require('./tests/experiment_test').test(server);
+    });
 
     // tests for Source API routes
     describe('Source API Routes', () => {
