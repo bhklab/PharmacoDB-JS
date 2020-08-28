f5b1cf7b19643039f00583bb95e418d017b945ef
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2020-08-28 13:21:55 -0400

    genes graphql query and initialized genes summary page.

--- client/src/Components/SummaryComponents/Genes/Genes.js
+++ client/src/Components/SummaryComponents/Genes/Genes.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
 import StyledWrapper from '../../../styles/utils';
 import Layout from '../../Utils/Layout';
+import Table from '../../Table/Table';
+import { getGenesQuery } from '../../../queries/gene';
 
 const Genes = () => (
   <Layout page="genes">
---
+++ client/src/queries/gene.js
@@ -0,0 +1,24 @@
+import { gql } from 'apollo-boost';
+
+/**
+ * @returns - Query returns the list of genes with the id and name of the gene
+ * and also returning the annotation object for each of the gene in the database.
+ */
+const getGenesQuery = gql`
+  query getAllGenes {
+    genes{
+      id
+      name
+      annotation {
+        ensg
+        gene_seq_start
+        gene_seq_end
+        
+      }
+    }
+  }
+`;
+
+export {
+  getGenesQuery,
+};
