5a08371ac8f1cf4e02dcb5f86e6728a406d836ee
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2020-08-28 21:50:50 -0400

    added a helper function to produce title case string.

---
+++ client/src/Components/Utils/TitleCase.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * @param {String} string - Takes string as an input.
+ * @returns {String} - returns a transformed string with the first letter capitalized for each word (Title Case/ Pascal Case).
+ */
+const TitleCase = (string) => {
+  const capitalString = string.split(' ').map((str) => {
+    if (str === 'and') {
+      return str;
+    }
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  });
+  return capitalString.join(' ');
+};
+
+export default TitleCase;
