4f3d7ec656f43f5efa201955f7db617b67572d9d
gangeshberiuhn <gangesh.beri@uhnresearch.ca>
2020-08-28 21:51:24 -0400

    cell line summary component.

--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -1,11 +1,12 @@
 import Compounds from './SummaryComponents/Compounds/Compounds';
 import Genes from './SummaryComponents/Genes/Genes';
 import Tissues from './SummaryComponents/Tissues/Tissues';
+import CellLines from './SummaryComponents/CellLines/CellLines';
 import IndivCompounds from './IndividualComponents/IndivCompounds/IndivCompounds';
 import Home from './Home/Home';
 import NotFoundPage from './Utils/NotFoundPage';
 
 export {
   Compounds, IndivCompounds, Home, NotFoundPage,
-  Tissues, Genes,
+  Tissues, Genes, CellLines,
 };
