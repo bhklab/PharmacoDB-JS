0d5f773da49656a21adcdd808132b9547a40810b
gangeshberi <gangeshberi@gmail.com>
2020-09-29 14:08:46 -0400

    Ongoing upset plot.

--- client/src/Components/Plots/UpsetPlot.js
+++ client/src/Components/Plots/UpsetPlot.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import * as d3 from 'd3';
+import BarPlot from './BarPlot'
 
 // margin for the svg element.
 const margin = {
@@ -10,14 +11,14 @@ const margin = {
 }
 
 // width and height of the SVG canvas.
-const width = 600 - margin.left - margin.right;
+const width = 1100 - margin.left - margin.right;
 const height = 600 - margin.top - margin.bottom;
 
 /**
  * creates an svg canvas for the plot.
  */
 const createSvg = () => {
-    d3.select('#upsetplot')
+    return d3.select('#upsetplot')
     .append('svg')
     .attr('width', width + margin.left + margin.right)
     .attr('height', height + margin.top + margin.bottom)
@@ -26,6 +27,20 @@ const createSvg = () => {
           'translate(' + margin.left + ',' + margin.top + ')');
 }
 
+/**
+ * creates the heatmap circles for the upset plot.
+ */
+const upsetCircle = (svg) => {
+    for(let i = 0; i < 10; i++) {
+        for(let j = 0; j < 10; j++) {
+            svg.append('circle')
+            .style('fill', 'silver')
+            .attr("r", 10)
+            .attr("cx", i * 30)
+            .attr("cy", j * 30);
+        }
+    }
+}
 
 /**
  * returns (
@@ -36,6 +51,8 @@ const UpsetPlot = () => {
     useEffect(() => {
         // svg canvas.
         const svg = createSvg()
+        // upset circle.
+        upsetCircle(svg)
     })
     return (
         <div id='upsetplot'/>
