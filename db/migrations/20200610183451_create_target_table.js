0f13a891be5de84fdd87004c7566f8e2db39e0b1
gangeshberi <gangeshberi@gmail.com>
2021-02-19 10:53:20 -0500

    updated migration files.

--- db/migrations/20200610183451_create_target_table.js
+++ db/migrations/20200610183451_create_target_table.js
@@ -1,19 +1,26 @@
-exports.up = function(knex) {
-    return knex.schema.hasTable('target').then(exists => {
-        if (!exists) {
-            return knex.schema.createTable('target', table => {
-                table.increments('id').primary();
-                table.string('name').notNullable();
-                table.integer('gene_id').unsigned();
-                table
-                    .foreign('gene_id')
-                    .references('id')
-                    .inTable('gene');
-            });
-        }
-    });
+exports.up = function (knex) {
+    return knex.schema.hasTable('target')
+        .then(exists => {
+            if (!exists) {
+                return knex.schema.createTable('target', table => {
+                    table.increments('id').primary();
+                    table.string('name').notNullable();
+                    table.integer('gene_id').unsigned();
+                    table
+                        .foreign('gene_id')
+                        .references('id')
+                        .inTable('gene');
+                });
+            }
+        })
+        .catch(err => {
+            throw err;
+        })
+        .finally(() => {
+            knex.destroy();
+        });
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
     return knex.schema.dropTableIfExists('target');
 };
