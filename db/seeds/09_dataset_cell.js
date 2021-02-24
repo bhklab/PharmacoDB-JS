68ef6de866c17c8af3e8010a73c911a48b5aa74d
unknown <evgeniya.a.gorobets@gmail.com>
2021-02-24 14:19:05 -0500

    Created and tested dataset_cell, dataset_tissue, dataset_compound seed files

---
+++ db/seeds/09_dataset_cell.js
@@ -0,0 +1,19 @@
+const csv = require('csvtojson');
+const path = require('path');
+
+const fileLocation = path.join('..', 'data', 'latest', 'dataset_cell.csv');
+const parserParams = { 
+    delimiter: ',', 
+    quote: '"',
+    ignoreEmpty: true
+};
+
+exports.seed = function (knex) {
+    return knex('dataset_cell').del()
+        .then(function () {
+            return csv(parserParams).fromFile(fileLocation);
+        })
+        .then(function (data) {
+            return knex('dataset_cell').insert(data);
+        });
+};
---
+++ db/seeds/11_dataset_compound.js
@@ -0,0 +1,19 @@
+const csv = require('csvtojson');
+const path = require('path');
+
+const fileLocation = path.join('..', 'data', 'latest', 'dataset_compound.csv');
+const parserParams = { 
+    delimiter: ',', 
+    quote: '"',
+    ignoreEmpty: true
+};
+
+exports.seed = function (knex) {
+    return knex('dataset_compound').del()
+        .then(function () {
+            return csv(parserParams).fromFile(fileLocation);
+        })
+        .then(function (data) {
+            return knex('dataset_compound').insert(data);
+        });
+};
