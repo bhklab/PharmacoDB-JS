098d964d9dab150584f42cb92bc93efe440bca44
parinaznasr <Isyfitms!23NFP>
2021-06-02 13:19:03 -0400

    update index

--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -4,11 +4,16 @@ import Tissues from './SummaryComponents/Tissues/Tissues';
 import Experiments from './SummaryComponents/Experiments/Experiments';
 import CellLines from './SummaryComponents/CellLines/CellLines';
 import IndivCompounds from './IndividualComponents/IndivCompounds/IndivCompounds';
+import IndivCellLines from './IndividualComponents/IndivCellLines/IndivCellLines';
+import IndivTissues from './IndividualComponents/IndivTissues/IndivTissues';
+import IndivDatasets from './IndividualComponents/IndivDatasets/IndivDatasets';
+import IndivGenes from './IndividualComponents/IndivGenes/IndivGenes';
 import Datasets from './SummaryComponents/Datasets/Datasets';
 import Home from './Home/Home';
-import NotFoundPage from './Utils/NotFoundPage';
+import NotFoundPage from './UtilComponents/NotFoundPage';
 
 export {
   Compounds, IndivCompounds, Home, NotFoundPage,
-  Tissues, Genes, CellLines, Datasets, Experiments,
+  Tissues, IndivTissues, Genes, IndivGenes, CellLines, IndivCellLines,
+  Datasets, IndivDatasets, Experiments,
 };
