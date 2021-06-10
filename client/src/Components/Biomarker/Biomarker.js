a5e0e4f0fafac0fa5cfe67680428add7ec7dd630
gangeshberi <gangeshberi@gmail.com>
2021-06-10 12:42:20 -0400

    Add: `biomarker` UI initial structure.

--- client/src/Components/Biomarker/Biomarker.js
+++ client/src/Components/Biomarker/Biomarker.js
@@ -1,12 +1,45 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { Link, Element } from 'react-scroll';
+import { getCompoundTarget } from '../../queries/target';
+import SnakeCase from '../../utils/convertToSnakeCase';
 import Layout from '../UtilComponents/Layout';
 import StyledWrapper from '../../styles/utils';
+import { StyledIndivPage, StyledSidebar } from '../../styles/IndivPageStyles';
+
+// side links.
+const SIDE_LINKS = ['Gene Information', 'Drug Information', 'Forest Plots']
+
+/**
+ *
+ * @param {String} link
+ */
+const createSideLink = (link) => <Link key={link} className="link" activeClass="selected" to={`${SnakeCase(link)}`} spy smooth duration={200} offset={-400}>{link}</Link>;
 
 const Biomarker = () => {
     return (
-        <Layout page="datasets">
+        <Layout>
             <StyledWrapper>
-                <h1> Biomarker </h1>
+                <StyledIndivPage>
+                    <StyledSidebar>
+                        {
+                            SIDE_LINKS.map((link) => createSideLink(link))
+                        }
+                    </StyledSidebar>
+                    <div className="container">
+                        <div className="content">
+                            <Element className="section" name="gene_information">
+                                <h3>Gene Information</h3>
+                            </Element>
+                            <Element className="section" name="drug_information">
+                                <h3>Drug Information</h3>
+                            </Element>
+                            <Element className="section" name="plots">
+                                <h3>Plots</h3>
+                            </Element>
+                        </div>
+                    </div>
+                </StyledIndivPage>
             </StyledWrapper>
         </Layout>
     );
