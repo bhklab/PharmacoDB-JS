226f4cb43b85b34f32bfb1b5005b4a181feff86c
gangeshberi <gangeshberi@gmail.com>
2021-06-10 16:20:29 -0400

    Add: `datasets list` util function.

---
+++ client/src/Components/Plots/ForestPlot.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ForestPlot = () => {
+    <h1> Hello </h1>
+}
+
+export default ForestPlot;
---
+++ client/src/utils/datasetsList.js
@@ -0,0 +1,6 @@
+/**
+ * @returns {Array} - an array of the dataset names in our database.
+ */
+const datasets = ['CTRPV2', 'GDSC1000', 'GRAY', 'FIMM', 'CCLE', 'gCSI', 'UHNBreast'];
+
+export default datasets;
