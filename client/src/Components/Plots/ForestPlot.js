5dc818f832dd8d17cc02ae2cafb61ccfe0b439cd
gangeshberi <gangeshberi@gmail.com>
2021-06-12 16:39:31 -0400

    Add: initial forest plot component.

--- client/src/Components/Plots/ForestPlot.js
+++ client/src/Components/Plots/ForestPlot.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import * as d3 from 'd3';
+import createSvgCanvas from '../../utils/createSvgCanvas';
 
-const ForestPlot = () => {
-    <h1> Hello </h1>
+// margin for the svg element.
+const margin = {
+    top: 20,
+    right: 20,
+    bottom: 20,
+    left: 20
+}
+
+// width and height of the SVG canvas.
+const width = 1100 - margin.left - margin.right;
+const height = 600 - margin.top - margin.bottom;
+
+/**
+ * creates the forest plot.
+ */
+const createForestPlot = (margin, height, width) => {
+    const svg = createSvgCanvas({ id: 'forestplot' })
+}
+
+/**
+ * @returns {component} - returns the forest plot component. 
+ */
+const ForestPlot = (props) => {
+    useEffect(() => {
+        createForestPlot(margin, height, width);
+    }, [])
+
+    return (
+        <div id="forestplot" />
+    )
 }
 
 export default ForestPlot;
