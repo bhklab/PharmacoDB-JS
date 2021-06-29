08f12c4691590119b378c460242636421f54eb5d
gangeshberi <gangeshberi@gmail.com>
2021-06-29 19:02:11 -0400

    Update: `package.json` to fix an issue.

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 import SearchContext from './context/SearchContext';
-const a;
 
 import {
     IndivCompounds,
