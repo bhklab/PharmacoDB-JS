31703ec6ae2e94fbd16ac14ab2e63b47491867b4
gangeshberi <gangeshberi@gmail.com>
2021-06-30 09:47:14 -0400

    Update: `knexfile.js`.

--- knexfile.js
+++ knexfile.js
@@ -26,7 +26,8 @@ module.exports = {
             host: process.env.DB_HOST,
             user: process.env.DB_USER,
             password: process.env.DB_PASS,
-            database: process.env.DB_NAME
+            database: process.env.DB_NAME,
+            port: process.env.DB_PORT
         },
         migrations: {
             directory: `${__dirname}/db/migrations`
