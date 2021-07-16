84ccd193978ac3342685a3626b3bb654feaf6f5a
gangeshberi <gangeshberi@gmail.com>
2021-07-16 00:27:28 -0400

    Fix: issue with the parsing of the data for the upset plot.

--- client/src/Components/Plots/UpsetPlot/ParseData.js
+++ client/src/Components/Plots/UpsetPlot/ParseData.js
@@ -63,14 +63,21 @@ const createUpsetPlotData = (data, subsets) => {
         if (subset.length > 0) {
             // union of the data.
             // subset.forEach(el => uniqueValues.push(...data[el]));
+
             // intersection
-            const result = subset.reduce((acc, cur) => {
-                if (typeof (acc) === "string") {
-                    return data[acc].filter((el) => data[cur].includes(el));
-                } else {
-                    return acc.filter((el) => data[cur].includes(el));
-                }
-            });
+            let result = [];
+            if (subset.length === 1) {
+                result = data[subset[0]];
+            } else {
+                result = subset.reduce((acc, cur) => {
+                    if (typeof (acc) === "string") {
+                        return data[acc].filter((el) => data[cur].includes(el));
+                    } else {
+                        return acc.filter((el) => data[cur].includes(el));
+                    }
+                });
+            }
+
             // append the object to final object variable.
             finalObject[`set${i}`] = {
                 keys: subset,
