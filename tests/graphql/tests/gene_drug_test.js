eeb0ee2dbee110c24d250e2a9c5d6a967870c581
gangeshberi <gangeshberi@gmail.com>
2021-07-22 15:26:58 -0400

    Fix: gene compound related existing API tests.

--- tests/graphql/tests/gene_drug_test.js
+++ tests/graphql/tests/gene_drug_test.js
@@ -6,6 +6,7 @@ const chai = require('chai');
 const expect = chai.expect;
 const request = require('supertest');
 const geneCompoundQueries = require('../queries/gene_drug_queries');
+const { isNullOrBoolean, isNullOrNumber } = require('./helper');
 
 /**
  * A function that contains tests for gene_compounds API.
@@ -13,23 +14,11 @@ const geneCompoundQueries = require('../queries/gene_drug_queries');
  */
 const test = (server) => {
     // test gene_compounds data for a given gene
-    it('Data coming from gene_compounds API route contains all experimental data for a single gene along with detailed information about dataset, tissue, gene and compound', function (done) {
+    it('Data coming from gene_compound API route contains all experimental data for a single gene along with detailed information about dataset, tissue, gene and compound', function (done) {
         this.timeout(10000);
         request(server)
             .post('/graphql')
             .send({ query: geneCompoundQueries.geneCompoundSearchByGeneQuery })
-            .expect(200)
-            .end((err, res) => {
-                if (err) return done(err);
-                testGeneCompoundData(res, done);
-            });
-    });
-    // test gene_compounds data for a given compound
-    it('Data coming from gene_compounds API route contains all experimental data for a single compound along with detailed information about dataset, tissue, gene and compound', function (done) {
-        this.timeout(10000);
-        request(server)
-            .post('/graphql')
-            .send({ query: geneCompoundQueries.geneCompoundSearchByCompoundQuery })
             .expect(200)
             .end((err, res) => {
                 if (err) return done(err);
@@ -43,42 +32,39 @@ const test = (server) => {
  * Takes 2 arguments, response request body and done callback function from chai
  */
 const testGeneCompoundData = (res, done) => {
-    const { gene_drugs } = res.body.data;
-    expect(gene_drugs).to.be.an('array').that.have.lengthOf.above(0);
-    gene_drugs.every(compound_drug => {
-        expect(compound_drug).to.have.all.keys('id', 'estimate', 'se', 'n', 'tstat', 'fstat', 'pvalue', 'df', 'fdr', 'FWER_genes', 'FWER_drugs', 'FWER_all', 'BF_p_all', 'mDataType', 'level', 'drug_like_molecule', 'in_clinical_trials', 'dataset', 'gene', 'compound', 'tissue');
-        const { id, estimate, se, n, tstat, fstat, pvalue, df, fdr, FWER_genes, FWER_drugs, FWER_all, BF_p_all, mDataType, level, drug_like_molecule, dataset, gene, compound, tissue } = compound_drug;
+    const { gene_compound_tissue } = res.body.data;
+    expect(gene_compound_tissue).to.be.an('array').that.have.lengthOf.above(0);
+    gene_compound_tissue.every(compound_drug => {
+        expect(compound_drug).to.have.all.keys('id', 'estimate', 'lower', 'upper', 'n', 'tstat', 'fstat', 'pvalue', 'df', 'fdr', 'FWER_gene', 'FWER_compound', 'FWER_all', 'BF_p_all', 'sens_stat', 'mDataType', 'tested_in_human_trials', 'in_clinical_trials', 'gene', 'compound', 'tissue');
+        const { id, estimate, lower, upper, n, tstat, fstat, pvalue, df, fdr, FWER_gene, FWER_compound, FWER_all, BF_p_all, mDataType, tested_in_human_trials, in_clinical_trials, gene, compound, tissue } = compound_drug;
         // checks the format of experimental data
         expect(id).to.be.a('number');
         expect(estimate).to.be.a('number');
-        expect(se).to.be.a('number');
-        expect(n).to.be.a('number');
-        expect(tstat).to.be.a('number');
-        expect(fstat).to.be.a('number');
+        expect(lower).to.be.a('number');
+        expect(upper).to.be.a('number');
+        expect(n).to.satisfy(isNullOrNumber);
+        expect(tstat).to.satisfy(isNullOrNumber);
+        expect(fstat).to.satisfy(isNullOrNumber);
         expect(pvalue).to.be.a('number');
-        expect(df).to.be.a('number');
-        expect(fdr).to.be.a('number');
-        expect(FWER_genes).to.be.a('number');
-        expect(FWER_drugs).to.be.a('number');
-        expect(FWER_all).to.be.a('number');
-        expect(BF_p_all).to.be.a('number');
+        expect(df).to.satisfy(isNullOrNumber);
+        expect(fdr).to.satisfy(isNullOrNumber);
+        expect(FWER_gene).to.be.a('number');
+        expect(FWER_compound).to.satisfy(isNullOrNumber);
+        expect(FWER_all).to.satisfy(isNullOrNumber);
+        expect(BF_p_all).to.satisfy(isNullOrNumber);
         expect(mDataType).to.be.a('string');
-        expect(level).to.be.a('number');
-        expect(drug_like_molecule).to.be.a('number');
+        expect(tested_in_human_trials).to.satisfy(isNullOrBoolean);
+        expect(in_clinical_trials).to.satisfy(isNullOrBoolean);
         // checks the format of gene object
         expect(gene).to.have.all.keys('id', 'name', 'annotation');
         expect(gene.id).to.be.a('number');
         expect(gene.name).to.be.string;
         expect(gene.annotation).to.be.an('object');
-        expect(gene.annotation).to.have.all.keys('gene_id', 'ensg', 'gene_seq_start', 'gene_seq_end');
+        expect(gene.annotation).to.have.all.keys('gene_id', 'gene_seq_start', 'gene_seq_end');
         // checks tissue data
         expect(tissue).to.have.all.keys('id', 'name');
         expect(tissue.id).to.be.a('number');
         expect(tissue.name).to.be.string;
-        // checks the format of dataset object
-        expect(dataset).to.have.all.keys('id', 'name');
-        expect(dataset.id).to.be.a('number');
-        expect(dataset.name).to.be.string;
         // checks the format of compound object
         expect(compound).to.have.all.keys('id', 'name', 'annotation');
         expect(compound.id).to.be.a('number');
@@ -89,6 +75,7 @@ const testGeneCompoundData = (res, done) => {
         // checks correct format of fda_status (database contains 0s or 1s)
         expect(compound.annotation.fda_status).to.be.oneOf(['Approved', 'Not Approved']);
     });
+
     return done();
 };
 
