3b955b8bb2c137e171d1f9163d068f3364b4e94b
gangeshberi <gangeshberi@gmail.com>
2021-08-14 00:32:25 -0400

    Update: `forestplot` according to the new data passed as props.

--- client/src/Components/Plots/ForestPlot.js
+++ client/src/Components/Plots/ForestPlot.js
@@ -4,49 +4,37 @@ import PropTypes from 'prop-types';
 import createSvgCanvas from '../../utils/createSvgCanvas';
 import colors from '../../styles/colors';
 
-const poly = [
-    { x: 250.0, y: 300.0 },
-    { x: 275, y: 275 },
-    { x: 300.0, y: 300.0 },
-    { x: 275.0, y: 325 },
-    { x: 250.0, y: 300.0 },
-];
 const data = [
     {
-        gene: 5512,
-        drug: 415,
-        tissue: 2,
-        dataset: 5,
-        n: 27,
-        se: 1.031,
-        estimate: -0.63,
+        gene: 64,
+        drug: 1,
+        tissue: 6,
+        dataset: 2,
+        n: 37,
+        lower_analytic: -0.468858800064676,
+        upper_analytic: 0.162209940075892,
+        upper_permutation: null,
+        lower_permutation: null,
+        estimate: -0.17,
         dataset_name: 'CCLE',
     },
     {
-        gene: 5512,
-        drug: 415,
-        tissue: 2,
+        gene: 64,
+        drug: 1,
+        tissue: 6,
         dataset: 3,
-        n: 5,
-        se: 0.203,
-        estimate: -0.085,
+        n: 39,
+        lower_analytic: -0.327969091537435,
+        upper_analytic: 0.302955393024951,
+        upper_permutation: null,
+        lower_permutation: null,
+        estimate: -0.0139,
         dataset_name: 'FIMM',
-    },
-    {
-        gene: 5512,
-        drug: 415,
-        tissue: 2,
-        dataset: 4,
-        n: 27,
-        se: 1.033,
-        estimate: 0.542,
-        dataset_name: 'gCSI',
-    },
+    }
 ];
 
 // data length and multiplier variables.
-const data_length = data.length + 3;
-const MULTIPLIER = 1.96;
+const data_length = data.length + 2;
 
 // margin for the svg element.
 const margin = {
@@ -64,30 +52,30 @@ const height = 400 - margin.top - margin.bottom;
 const minEstimate = Math.min(...data.map((val) => val.estimate));
 const maxEstimate = Math.max(...data.map((val) => val.estimate));
 
-// calculates the minimum and maximum standard error from the data.
-const minStandardError = Math.min(...data.map((val) => val.se));
-const maxStandardError = Math.max(...data.map((val) => val.se));
+// calculates the minimum and maximum analytic from the data.
+const minAnalytic = Math.min(...data.map((val) => val.lower_analytic));
+const maxAnalytic = Math.max(...data.map((val) => val.upper_analytic));
 
-// calculates the minimum and maximum n value from the data.
-const minNValue = Math.min(...data.map((val) => val.n));
-const maxNValue = Math.max(...data.map((val) => val.n));
+// calculates the minimum and maximum permutation from the data.
+const minPermutation = Math.min(...data.map((val) => val.lower_permutation));
+const maxPermutation = Math.max(...data.map((val) => val.upper_permutation));
+
+// assign min and max.
+const min = minPermutation || minAnalytic;
+const max = maxPermutation || maxAnalytic;
 
 /**
  * @returns - d3 linear scale for circles.
  * mapped the min and max values to a range.
  */
-const circleScaling = () => d3.scaleLinear().domain([0, 150]).range([5, 15]);
+const circleScaling = () => d3.scaleLinear().domain([0, 150]).range([5, 25]);
 
 /**
  * @returns - d3 linear scale for x-axis.
  */
 const createXScale = () =>
-    d3
-        .scaleLinear()
-        .domain([
-            minEstimate - MULTIPLIER * maxStandardError,
-            maxEstimate + MULTIPLIER * maxStandardError,
-        ])
+    d3.scaleLinear()
+        .domain([min, max])
         .range([100, (width * 3) / 4])
         .nice();
 
@@ -130,10 +118,10 @@ const createHorizontalLines = (svg, scale) => {
             .append('line')
             .style('stroke', `${colors.dark_gray_text}`)
             .style('stroke-width', 1.5)
-            .attr('x1', scale(element.estimate - MULTIPLIER * element.se))
-            .attr('y1', (i * height) / data_length + height / data_length)
-            .attr('x2', scale(element.estimate + MULTIPLIER * element.se))
-            .attr('y2', (i * height) / data_length + height / data_length);
+            .attr('x1', scale(element.lower_permutation || element.lower_analytic))
+            .attr('y1', ((i + 1) * height) / data_length)
+            .attr('x2', scale(element.upper_permutation || element.upper_analytic))
+            .attr('y2', ((i + 1) * height) / data_length)
     });
 };
 
@@ -149,7 +137,7 @@ const createCircles = (svg, xScale, circleScale) => {
             .attr('id', `cirlce-${element.dataset}`)
             .append('circle')
             .attr('cx', xScale(element.estimate))
-            .attr('cy', (i * height) / data_length + height / data_length)
+            .attr('cy', ((i + 1) * height) / data_length)
             .attr('r', circleScale(element.n))
             .attr('stroke', 'black')
             .attr('fill', `${colors.teal}`);
@@ -161,21 +149,21 @@ const createCircles = (svg, xScale, circleScale) => {
  * @param {Object} svg - svg selection for the global canvas.
  * @param {Object} scale - x axis scale.
  */
-const createPolygon = (svg, scale) => {
-    const lineFunction = d3
-        .line()
-        .x(function (d) {
-            return d.x;
-        })
-        .y(function (d) {
-            return d.y;
-        });
+// const createPolygon = (svg, scale) => {
+//     const lineFunction = d3
+//         .line()
+//         .x(function (d) {
+//             return d.x;
+//         })
+//         .y(function (d) {
+//             return d.y;
+//         });
 
-    svg.append('path')
-        .attr('d', lineFunction(poly))
-        .attr('stroke', `${colors.dark_gray_text}`)
-        .attr('fill', `${colors.teal}`);
-};
+//     svg.append('path')
+//         .attr('d', lineFunction(poly))
+//         .attr('stroke', `${colors.dark_gray_text}`)
+//         .attr('fill', `${colors.teal}`);
+// };
 
 /**
  * Appends dataset name to the right of the forest plot.
@@ -189,9 +177,9 @@ const appendDatasetName = (svg) => {
         .attr('font-weight', 700)
         .attr('x', 10)
         .attr('y', 0)
-        .attr('fill', `${colors.dark_gray_text}`)
+        .attr('fill', `${colors.dark_teal_heading}`)
         .text('Dataset Name')
-        .attr('font-size', '22px');
+        .attr('font-size', '20px');
 
     // append dataset name.
     data.forEach((element, i) => {
@@ -200,10 +188,10 @@ const appendDatasetName = (svg) => {
             .append('text')
             .attr('font-weight', 200)
             .attr('x', 10)
-            .attr('y', (i * height) / data_length + height / data_length)
-            .attr('fill', `${colors.dark_gray_text}`)
+            .attr('y', ((i + 1) * height) / data_length)
+            .attr('fill', `${colors.dark_teal_heading}`)
             .text(`${element.dataset_name}`)
-            .attr('font-size', '18px');
+            .attr('font-size', '16px');
     });
 };
 
@@ -219,9 +207,9 @@ const appendEstimateText = (svg) => {
         .attr('font-weight', 700)
         .attr('x', (width * 3) / 4 + 10)
         .attr('y', 0)
-        .attr('fill', `${colors.dark_gray_text}`)
+        .attr('fill', `${colors.dark_teal_heading}`)
         .text('Estimate')
-        .attr('font-size', '22px');
+        .attr('font-size', '20px');
 
     // append dataset name.
     data.forEach((element, i) => {
@@ -231,12 +219,12 @@ const appendEstimateText = (svg) => {
             .append('text')
             .attr('font-weight', 200)
             .attr('x', (width * 3) / 4 + 10)
-            .attr('y', (i * height) / data_length + height / data_length)
-            .attr('fill', `${colors.dark_gray_text}`).text(`(
-                ${(element.estimate - MULTIPLIER * element.se).toFixed(2)}, 
-                ${(element.estimate + MULTIPLIER * element.se).toFixed(2)}
+            .attr('y', ((i + 1) * height) / data_length)
+            .attr('fill', `${colors.dark_teal_heading}`).text(`(
+                ${(element.lower_permutation || element.lower_analytic).toFixed(2)}, 
+                ${(element.upper_permutation || element.upper_analytic).toFixed(2)}
             )`)
-            .attr('font-size', '18px');
+            .attr('font-size', '16px');
     });
 };
 
@@ -245,8 +233,9 @@ const appendEstimateText = (svg) => {
  * @param {Object} margin - margin for the svg canavas.
  * @param {number} height - height of the svg canvas.
  * @param {number} width - width of the svg canvas.
+ * @param {Array} data - array of data.
  */
-const createForestPlot = (margin, height, width) => {
+const createForestPlot = (margin, height, width, data) => {
     // creating the svg canvas.
     const svg = createSvgCanvas({ id: 'forestplot', width, height, margin });
     // scale for x-axis.
@@ -272,9 +261,9 @@ const createForestPlot = (margin, height, width) => {
 /**
  * @returns {component} - returns the forest plot component.
  */
-const ForestPlot = (props) => {
+const ForestPlot = ({ data }) => {
     useEffect(() => {
-        createForestPlot(margin, height, width);
+        createForestPlot(margin, height, width, data);
     }, []);
 
     return <div id="forestplot" />;
