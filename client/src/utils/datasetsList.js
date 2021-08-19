a16720536d761bf5cbf90f3602d14ee84847a05f
gangeshberi <gangeshberi@gmail.com>
2021-08-18 22:47:43 -0400

    Update: `datasetList.js` to include the missing datasets.

--- client/src/utils/datasetsList.js
+++ client/src/utils/datasetsList.js
@@ -2,6 +2,6 @@
  * 
  * @returns {Array} - an array of the dataset names in our database.
  */
-const datasets = ['CTRPV2', 'GDSC1000', 'GRAY', 'FIMM', 'CCLE', 'gCSI', 'UHNBreast'];
+const datasets = ['CTRPV2', 'GDSC_v1', 'GDSC_v2', 'GRAY', 'FIMM', 'CCLE', 'gCSI', 'UHNBreast'];
 
 export default datasets;
