4e89719262e333a0c555f7c499456e44853296ff
gangeshberi <gangeshberi@gmail.com>
2021-08-23 14:12:41 -0400

    Fix: an issue with rendering the correct page in `IntersectionMain.js`

--- client/src/Components/IntersectionComponents/IntersectionMain.js
+++ client/src/Components/IntersectionComponents/IntersectionMain.js
@@ -32,10 +32,13 @@ const IntersectionMain = () => {
             if (keys.includes('tissue')) {
                 pageName = 'tissueCompound';
             }
-        } else if (keys.length === 2 && keys.includes('dataset_intersection')) {
-            pageName = 'datasetIntersection';
-        } else if (keys.length === 1 && keys.includes('dataset_intersection')) { //TODO: update this to render individual dataset page.
-            pageName = 'individualDataset';
+        } else if (keys.includes('dataset_intersection')) {
+            const value = values['dataset_intersection'].split(',');
+            if (value.length === 1) {
+                pageName = 'individualDataset';
+            } else if (value.length >= 2) {
+                pageName = 'datasetIntersection';
+            }
         } else {
             pageName = 'notFound';
         }
