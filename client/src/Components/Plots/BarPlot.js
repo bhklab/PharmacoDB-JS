4c55a1384f8eafa24d9dfffd4884cbc5dc31b2c8
parinaznasr <parinaz.nasresfahani@gmail.com>
2021-09-12 14:21:42 -0400

    fixed compound summary page plot

--- client/src/Components/Plots/BarPlot.js
+++ client/src/Components/Plots/BarPlot.js
@@ -14,8 +14,8 @@ const layout = {
   },
   yaxis: {
     type: 'log',
-    // tickmode: 'linear',
-    tickformat: '.1r',
+    tickvals : [0, 10, 100, 1000, 10000, 50000, 100000, 500000, 1000000, 5000000, 10000000],
+    ticktext : [0, 10, 100, 1000, '10k', '50k', '100k', '500k', '1M', '5M', '10M'],
   },
 };
 
