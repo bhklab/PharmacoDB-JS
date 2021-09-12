16632ec42b3f79a24e8e22f93acc1197fb183c89
gangeshberi <gangeshberi@gmail.com>
2021-09-12 13:45:13 -0400

    Update: gene table.

--- client/src/Components/SummaryComponents/Genes/GenesTable.js
+++ client/src/Components/SummaryComponents/Genes/GenesTable.js
@@ -7,78 +7,78 @@ import Loading from '../../UtilComponents/Loading';
 import Error from '../../UtilComponents/Error';
 
 const table_columns = [
-    {
-      Header: 'Name',
-      accessor: 'name',
-      Cell: (row) => (<Link to={`/genes/${row.row.original.id}`}>{row.value}</Link>),
-    },
-    {
-        Header: 'Symbol',
-        accessor: 'symbol',
-    },
-    // {
-    //   Header: 'Gene Seq Start',
-    //   accessor: 'gene_seq_start',
-    // },
-    // {
-    //   Header: 'Gene Seq End',
-    //   accessor: 'gene_seq_end',
-    // },
+  {
+    Header: 'Ensembl ID',
+    accessor: 'name',
+    Cell: (row) => (<Link to={`/genes/${row.row.original.id}`}>{row.value}</Link>),
+  },
+  {
+    Header: 'Symbol',
+    accessor: 'symbol',
+  },
+  // {
+  //   Header: 'Gene Seq Start',
+  //   accessor: 'gene_seq_start',
+  // },
+  // {
+  //   Header: 'Gene Seq End',
+  //   accessor: 'gene_seq_end',
+  // },
 ];
 
 /**
  *
  * @param {Array} data - gene data from the genes API call.
  */
- const getTableData = (data) => {
-    let table_data = [];
-    if (data) {
-      table_data = data.genes.map((value) => {
-        const { name, annotation, id } = value;
-        const { symbol, ensg, gene_seq_end, gene_seq_start } = annotation;
-        return {
-          id,
-          // name: symbol ? symbol : name,
-          name: name,
-          symbol: symbol ? symbol : "N/A",
-          ensg,
-          gene_seq_start,
-          gene_seq_end,
-        };
-      });
-    }
-    table_data.sort((a, b) => a.name.localeCompare(b.name));
-    return table_data;
+const getTableData = (data) => {
+  let table_data = [];
+  if (data) {
+    table_data = data.genes.map((value) => {
+      const { name, annotation, id } = value;
+      const { symbol, ensg, gene_seq_end, gene_seq_start } = annotation;
+      return {
+        id,
+        // name: symbol ? symbol : name,
+        name: name,
+        symbol: symbol ? symbol : "N/A",
+        ensg,
+        gene_seq_start,
+        gene_seq_end,
+      };
+    });
+  }
+  table_data.sort((a, b) => a.name.localeCompare(b.name));
+  return table_data;
 };
 
 const GenesTable = () => {
 
-    const [genes, setGenes] = useState([]);
-    const [error, setError] = useState(false);
+  const [genes, setGenes] = useState([]);
+  const [error, setError] = useState(false);
 
-    const { loading } = useQuery(getGenesQuery, {
-        onCompleted: (data) => {
-            setGenes(getTableData(data));
-        },
-        onError: (err) => {
-          console.log(err);
-          setError(true);
-        }
-    });
+  const { loading } = useQuery(getGenesQuery, {
+    onCompleted: (data) => {
+      setGenes(getTableData(data));
+    },
+    onError: (err) => {
+      console.log(err);
+      setError(true);
+    }
+  });
 
-    return(
-        <React.Fragment>
-            <h3>List of Genes</h3>
-            {
-                loading ? <Loading />
-                :
-                error ? <Error />
-                :
-                genes.length > 0 &&
-                <Table columns={table_columns} data={genes} />
-            }
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <h3>List of Genes</h3>
+      {
+        loading ? <Loading />
+          :
+          error ? <Error />
+            :
+            genes.length > 0 &&
+            <Table columns={table_columns} data={genes} />
+      }
+    </React.Fragment>
+  )
 }
 
 export default GenesTable;
