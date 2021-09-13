abc4cc7894bf612d57b5d802fd095d06936af1e4
parinaznasr <parinaz.nasresfahani@gmail.com>
2021-09-12 23:32:43 -0400

    removed dataset changes

--- client/src/Components/IntersectionComponents/DatasetIntersection/DatasetIntersection.js
+++ client/src/Components/IntersectionComponents/DatasetIntersection/DatasetIntersection.js
@@ -143,6 +143,7 @@ const renderComponent = (cellDataLoading, datasetDataLoading, parsedCellData, up
     } else {
         return (
             <>
+                {/*<h2>{datasetString}</h2>*/}
                 <h2>List of Datasets</h2>
                 <UpsetPlot data={parsedCellData} datasets={updatedDatasets} />
             </>
--- client/src/Components/SummaryComponents/Datasets/Datasets.js
+++ client/src/Components/SummaryComponents/Datasets/Datasets.js
@@ -1,14 +1,12 @@
-import React , {useState}from 'react';
+import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import Table from '../../UtilComponents/Table/Table';
 import { Link } from 'react-router-dom';
-import Select from 'react-select';
 import Layout from '../../UtilComponents/Layout';
 import { getDatasetsQuery } from '../../../queries/dataset';
 import StyledWrapper from '../../../styles/utils';
 import Loading from '../../UtilComponents/Loading';
 import DatasetIntersection from '../../IntersectionComponents/DatasetIntersection/DatasetIntersection';
-import StyledSelectorContainer from '../../../styles/Utils/StyledSelectorContainer';
 
 // an array with the columns of dataset table.
 const table_columns = [
@@ -36,11 +34,11 @@ const renderComponent = (loading, error, columns, data) => {
     return <p> Error! </p>;
   }
   return (
-    <React.Fragment>
-      <DatasetIntersection />
-      <h2>Dataset names</h2>
-      <Table columns={columns} data={data} center={true}/>
-    </React.Fragment>
+      <>
+        <DatasetIntersection />
+        <h2>Dataset names</h2>
+        <Table columns={columns} data={data} />
+      </>
   );
 };
 
@@ -53,32 +51,19 @@ const renderComponent = (loading, error, columns, data) => {
  * returns (
  *   <Datasets/>
  * )
-*/
+ */
 const Datasets = () => {
   const { loading, error, data } = useQuery(getDatasetsQuery);
   const columns = React.useMemo(() => table_columns, []);
   const dataset_data = React.useMemo(() => (data ? data.datasets : []), [data]);
-  const TypeOptions = ['Cell Line', 'Compound', 'Tissue'];
-  const [selectedDataset, setSelectedDataset] = useState('Cell Line');
   return (
-    <Layout page="datasets">
-      <StyledWrapper>
-        <StyledSelectorContainer>
-          <div className="selector-container">
-            <div className='label'>Dataset:</div>
-            <Select
-                className='selector'
-                defaultValue={{ value: selectedDataset, label: selectedDataset }}
-                options={TypeOptions}
-                onChange={(e) => setSelectedDataset(e.value)}
-            />
-          </div>
-        </StyledSelectorContainer>
-        {
-          renderComponent(loading, error, columns, dataset_data)
-        }
-      </StyledWrapper>
-    </Layout>
+      <Layout page="datasets">
+        <StyledWrapper>
+          {
+            renderComponent(loading, error, columns, dataset_data)
+          }
+        </StyledWrapper>
+      </Layout>
   );
 };
 
