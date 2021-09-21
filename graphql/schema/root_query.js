e4ab7e26086bb1b3ad5fb342253b986daba8c291
gangeshberi <gangeshberi@gmail.com>
2021-09-21 16:30:55 -0400

    new root queries related to target.

--- graphql/schema/root_query.js
+++ graphql/schema/root_query.js
@@ -111,10 +111,10 @@ const RootQuery = `type RootQuery {
     """
         Root Queries for targets.
     """
-    compound_target(compoundId: Int, compoundName: String): CompoundTarget!
-    gene_compound_target(compoundId: Int, compoundName: String): GeneCompoundTarget!
-    compound_targets(page: Int, per_page: Int, all: Boolean): [CompoundTarget]
-    compounds_gene_target(geneId: Int, geneName: String): CompoundsGeneTarget!
+    single_compound_target(compoundId: Int, compoundName: String): CompoundTarget
+    single_gene_target(geneId: Int, geneName: String): GeneTarget
+    gene_compound_target(compoundId: Int, compoundName: String): GeneCompoundTarget!    
+    compounds_gene_target(geneId: Int, geneName: String): CompoundsGeneTarget!    
     
     """
         Root Query for the stats for the different types.
