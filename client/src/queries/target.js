26605506e9aaa4378cf74e5597a0d128497729d5
gangeshberi <gangeshberi@gmail.com>
2021-09-24 09:01:04 -0400

    Add: new client side queries for single compound and single gene `target` queries.

--- client/src/queries/target.js
+++ client/src/queries/target.js
@@ -1,32 +1,16 @@
 import { gql } from 'apollo-boost';
 
 
-/**
- * @param {number} compoundId - compound id of the compound to be queried.
- * @returns - Query returns an object for the compound target based on the compound id.
- */
-const getCompoundTarget = gql`
-    query getCompoundTarget($compoundId: Int, $compoundName: String) {
-        compound_target(compoundId: $compoundId, compoundName: $compoundName) {
-            compound_id,
-            compound_name,
+const getSingleCompoundTarget = gql`
+    query getSingleCompoundTarget($compoundId: Int, $compoundName: String) {
+        single_compound_target(compoundId: $compoundId, compoundName: $compoundName) {
+            compound_id
+            compound_name
+            compound_uid
             targets {
-                id
-                name
-            }
-        }
-    }
-`;
-
-const getGeneCompountTarget = gql`
-    query getGeneCompoundTarget($compoundId: Int, $compoundName: String) {
-        gene_compound_target(compoundId: $compoundId, compoundName: $compoundName) {
-            compound_id,
-            compound_name,
-            targets {
-                id
-                name
-                gene {
+                target_id
+                target_name
+                genes {
                     id
                     name
                     annotation {
@@ -38,31 +22,29 @@ const getGeneCompountTarget = gql`
     }  
 `;
 
-const getCompountsGeneTarget = gql`
-    query getCompountsGeneTarget($geneId: Int, $geneName: String) {
-        compounds_gene_target(geneId: $geneId, geneName: $geneName) {
-            gene {
-                id
-                name
-                annotation {
-                    symbol
-                }
+const getSingleGeneTarget = gql`
+    query getSingleGeneTarget($geneId: Int, $geneName: String) {
+        single_gene_target(geneId: $geneId, geneName: $geneName) {
+            gene_id
+            gene_name
+            gene_annotation {
+                symbol
             }
-            compounds {
-                compound_id,
-                compound_name,
-                compound_uid,
-                targets  {
-                        id,
-                        name
+            targets {
+                target_id
+                target_name
+                compounds {
+                    id
+                    name
+                    uid
                 }
             }
         }
     }  
 `;
 
+
 export {
-    getCompoundTarget,
-    getGeneCompountTarget,
-    getCompountsGeneTarget
+    getSingleCompoundTarget,
+    getSingleGeneTarget,
 };
