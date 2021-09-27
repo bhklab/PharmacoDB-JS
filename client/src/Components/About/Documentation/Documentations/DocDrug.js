038294883df761f85db21249f3522cdc8680d298
parinaznasr <parinaz.nasresfahani@gmail.com>
2021-09-27 15:52:31 -0400

    updated documentations with compounds

--- client/src/Components/About/Documentation/Documentations/DocDrug.js
+++ client/src/Components/About/Documentation/Documentations/DocDrug.js
@@ -23,38 +23,38 @@ import pacliTarget from '../../../../images/DocumentationImages/pacli_target.png
 const DocDrug = () => {
     return(
         <div className='documentation'>
-            <p>Each drug included in PharmacoDB has a webpage page associated with it. These pages can be accessed by searching for a drug through the search bar, or by clicking on the compounds link at the bottom of the homepage or from the Data drop down menu on the top right side of the homepage.</p>
+            <p>Each compound included in PharmacoDB has a webpage page associated with it. These pages can be accessed by searching for a compound through the search bar, or by clicking on the compounds link at the bottom of the homepage or from the Data drop down menu on the top right side of the homepage.</p>
             <div className="center"><img src={drugSearch}/></div>
             <div className="center"><p>Or</p></div>
             <div className="center"><img src={homeCompound}/></div>
-            <p>Compounds link directs you to a page starting with a bar plot showing the number of drugs tested in each dataset included in PharmacoDB. Hovering over each bar shows the exact number of drugs.</p>
+            <p>Compounds link directs you to a page starting with a bar plot showing the number of compounds tested in each dataset included in PharmacoDB. Hovering over each bar shows the exact number of compounds.</p>
             <div className="center"><img src={drugSummaryBar}/></div>
-            <p>The page also contains a table showing the name, SMILES, InChIKeys, PubChem name, and ChEMBL and FDA status of all the drugs included in the PharmacoDB. Clicking on each drug name will take you to the corresponding drug page.</p>
+            <p>The page also contains a table showing the name, SMILES, InChIKeys, PubChem name, and ChEMBL and FDA status of all the compounds included in the PharmacoDB. Clicking on each compound name will take you to the corresponding compound page.</p>
             {/*<img width="600px" alt="compound summary page- list of compounds" src={doseImg} title="Compounds"/>*/}
-            <p>Each drug page has a sidebar including Synonyms and IDS, Annotated Targets, Bar Plots, AAC (Cell Lines), AAC (Tissues), Cell Line Summary, Tissue Summary, and Molecular Features.</p>
-            <h7>Synonyms and IDS</h7>
-            <p>Synonyms and IDS page displays information about the datasets which performed experiments with the drug of query as well as the name each gave to it. External identifiers such as SMILES, InChiKey and PubChem ID are also available on this page. Clicking on PubChem ID will direct you to the corresponding page on PubChem.</p>
+            <p>Each compound page has a sidebar including Synonyms and IDS, Annotated Targets, Bar Plots, AAC (Cell Lines), AAC (Tissues), Cell Line Summary, Tissue Summary, and Molecular Features.</p>
+            <h7>Annotations</h7>
+            <p>Annotations page displays information about the datasets which performed experiments with the compound of query as well as the name each gave to it. External identifiers such as SMILES, InChiKey and PubChem ID are also available on this page. Clicking on PubChem ID will direct you to the corresponding page on PubChem.</p>
             <div className="center"><img className="small" src={pacliSynonym}/></div>
             <h7>Annotated Targets</h7>
-            <p>Annotated Targets page shows a table which includes the name of drug targets, genes associated with the targets, and Ensembl IDs of the genes.</p>
+            <p>Annotated Targets page shows a table which includes the name of compound targets, genes associated with the targets, and Ensembl IDs of the genes.</p>
             <div className="center"><img className="small" src={pacliTarget}/></div>
             <h7>Bar Plots</h7>
-            <p>Bar Plots page shows bar plots comparing the number of cells and tissues tested on with that drug in each PharmacoDB dataset. Hovering over each bar shows the exact number of cells/tissues for that dataset.</p>
+            <p>Bar Plots page shows bar plots comparing the number of cells and tissues tested on with that compound in each PharmacoDB dataset. Hovering over each bar shows the exact number of cells/tissues for that dataset.</p>
             <div className="center"><img className="small" src={drugIndBar}/></div>
             <h7>AAC (Cell Lines)</h7>
-            <p>AAC (Cell Lines) page shows a waterfall plot displaying the most and least sensitive cell lines tested with the drug. There is a profile selector to show the AAC or IC50 values, and a dataset selector to show only the experiments in the desired dataset. Hovering over each bar shows the name of the cell line, and the AAC/IC50 of the experiment.</p>
+            <p>AAC (Cell Lines) page shows a waterfall plot displaying the most and least sensitive cell lines tested with the compound. There is a profile selector to show the AAC or IC50 values, and a dataset selector to show only the experiments in the desired dataset. Hovering over each bar shows the name of the cell line, and the AAC/IC50 of the experiment.</p>
             <div className="center"><img className="small" src={drugAACCell}/></div>
             <h7>AAC (Tissues)</h7>
-            <p>AAC (Tissues) page shows a box plot displaying the sensitivity range of the tissues tested with the drug across all datasets. Each dot represents a cell line and similarly there is a profile selector to show the AAC/IC50 values, and a dataset selector to show only the experiments in the desired dataset. Hovering over each box shows the tissue name, minimum, maximum, sample median, and the first and third quartiles of the AAC/IC50 of the experiment for that tissue.</p>
+            <p>AAC (Tissues) page shows a box plot displaying the sensitivity range of the tissues tested with the compound across all datasets. Each dot represents a cell line and similarly there is a profile selector to show the AAC/IC50 values, and a dataset selector to show only the experiments in the desired dataset. Hovering over each box shows the tissue name, minimum, maximum, sample median, and the first and third quartiles of the AAC/IC50 of the experiment for that tissue.</p>
             <div className="center"><img className="small" src={drugAACTissue}/></div>
-            <h7>Cell Line Summary</h7>
-            <p>Cell Line Summary page includes a table of cells and tissues on which that drug has been tested, with the studies in which the cell - drug combination can be found and the total number of experiments across all datasets. Clicking on each cell line, tissue or dataset name will take you to the corresponding page.</p>
+            <h7>Cell Lines Summary</h7>
+            <p>Cell Lines Summary page includes a table of cells and tissues on which that compound has been tested, with the studies in which the cell - compound combination can be found and the total number of experiments across all datasets. Clicking on each cell line, tissue or dataset name will take you to the corresponding page.</p>
             <div className="center"><img className="small" src={drugIndCellList}/></div>
-            <h7>Tissue Summary</h7>
-            <p>Tissue Summary page includes a table of tissue on which that drug has been tested, with the studies in which the drug - tissue combination can be found and the total number of experiments across all cell lines of the tissue type in all datasets. Similarly, clicking on tissue,  dataset names, and experiment numbers will take you to the corresponding page.</p>
+            <h7>Tissues Summary</h7>
+            <p>Tissues Summary page includes a table of tissue on which that compound has been tested, with the studies in which the compound - tissue combination can be found and the total number of experiments across all cell lines of the tissue type in all datasets. Similarly, clicking on tissue,  dataset names, and experiment numbers will take you to the corresponding page.</p>
             <div className="center"><img className="small" src={drugIndTissues}/></div>
             <h7>Molecular Features</h7>
-            <p>Molecular Features page shows a table summarizing the top molecular features associated with the drug.</p>
+            <p>Molecular Features page shows a table summarizing the top molecular features associated with the compound.</p>
             {/*<img width="600px" alt="compound indiv page- Molecular feature table" src={doseImg} title="Molecular feature"/>*/}
         </div>
     );
--- client/src/Components/About/Documentation/Documentations/DocSearch.js
+++ client/src/Components/About/Documentation/Documentations/DocSearch.js
@@ -19,15 +19,15 @@ const DocSearch = () => {
     return(
         <div className='documentation'>
             <p>The main way to interact with PharmacoDB is through its Search interface. The search bar, found on the homepage of the website, allows the user to query all the data contained in the database, and functions as the main navigation tool around the web app.</p>
-            <p>The Search bar takes several distinct types of queries. The simplest is to query for a single specific entity in the database. One can use the search to query for a specific cell (e.g. MCF-7), drug (e.g. 17-AAG), tissue (e.g. breast) or dataset (e.g. CTRPv2). This query would then take you to the corresponding page for that instance. It is feasible to search for the synonyms of a drug name or cell names as long as they are included in one of the PharmacoDB datasets.</p>
-            <p>The search bar also allows the user to specify a data type (e.g. cell, drug, dataset), where it would take the user to a page listing all instances of that data type in the database.</p>
+            <p>The Search bar takes several distinct types of queries. The simplest is to query for a single specific entity in the database. One can use the search to query for a specific cell (e.g. MCF-7), compound (e.g. 17-AAG), tissue (e.g. breast) or dataset (e.g. CTRPv2). This query would then take you to the corresponding page for that instance. It is feasible to search for the synonyms of a compound name or cell names as long as they are included in one of the PharmacoDB datasets.</p>
+            <p>The search bar also allows the user to specify a data type (e.g. cell, compound, dataset), where it would take the user to a page listing all instances of that data type in the database.</p>
             {/*<img width="650px" alt="Searching cell line data" src={logo} title="Searching cell"/>*/}
-            <p>Queries consisting of the names of multiple datasets, with a space between dataset names, are also allowed. This query will link the user to a page describing the intersection between the datasets, with tables listing the common cells, drugs and tissue types.</p>
+            <p>Queries consisting of the names of multiple datasets, with a space between dataset names, are also allowed. This query will link the user to a page describing the intersection between the datasets, with tables listing the common cells, compounds and tissue types.</p>
             <div className="center"><img src={intersectionSearch}/></div>
-            <p>Queries consisting of combinations of the name of a drug with cell line or tissue, delimited by a space, will direct to a page plotting all the available drug dose response curves for that combination across all datasets in PharmacoDB.</p>
-            <h6>Cell line vs. Drug/ Drug vs. Cell line</h6>
+            <p>Queries consisting of combinations of the name of a compound with cell line or tissue, delimited by a space, will direct to a page plotting all the available compound dose response curves for that combination across all datasets in PharmacoDB.</p>
+            <h6>Cell line vs. Compound/ Compound vs. Cell line</h6>
             <div className="center"><img src={cellDrugSearch}/></div>
-            <h6>Drug vs. Tissue/ Tissue vs. Drug</h6>
+            <h6>Compound vs. Tissue/ Tissue vs. Compoundg</h6>
             <div className="center"><img src={drugTissueSearch}/></div>
             <p>Another main way of interacting with PharmacoDB is by clicking on the data types provided at the bottom of the homepage. Data types are also accessible through the Data drop down menu on the top right side of the homepage.</p>
             <div className="center"><img src={homeNav}/></div>
