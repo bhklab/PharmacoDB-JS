516cff4e114ce5946ebd19b6cc1758edc3d80cc7
gangeshberi <gangeshberi@gmail.com>
2021-10-13 13:56:23 -0400

    Add: new query to `getTissueIdBasedOnTissueName`.

--- client/src/queries/tissue.js
+++ client/src/queries/tissue.js
@@ -14,7 +14,19 @@ const getTissuesQuery = gql`
 `;
 
 /**
- * @param { number } tissueId - tissue id of the cell line to be queried.
+ * @param {string} tissueName - name of the tissue.
+ */
+const getTissueIdBasedOnTissueName = gql`
+  query getTissueIdBasedOnTissueName($tissueName: String!) {
+    tissue(tissueName: $tissueName) {
+      id
+      name
+    }
+  }
+`;
+
+/**
+ * @param { number } tissueId - tissue id of the tissue to be queried.
  */
 const getTissueQuery = gql`
   query getSingleTissue($tissueId: Int!) {
@@ -48,4 +60,5 @@ const getTissueQuery = gql`
 export {
   getTissuesQuery,
   getTissueQuery,
+  getTissueIdBasedOnTissueName,
 };
