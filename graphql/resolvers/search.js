9e817dc220250c1d36cefecb2a0c1c3ad2bbfdcd
gangeshberi <gangeshberi@gmail.com>
2022-11-18 00:19:36 -0500

    Update: `search` resolver function.

--- graphql/resolvers/search.js
+++ graphql/resolvers/search.js
@@ -1,19 +1,27 @@
 const knex = require('../../db/knex');
+const { getGenesBasedOnSymbol } = require('./gene');
 
-const getGenesBasedOnSymbol = (symbol) => knex.select('gene.id', 'symbol as value')
-    .from('gene')
-    .join('gene_annotation', 'gene.id', 'gene_annotation.gene_id')
-    .where('symbol', 'like', `%${symbol}%`);
 
+/**
+ * 
+ * @param {Array} data 
+ * @param {string} type 
+ * @returns {Array} - array of object with updated object with addition of type.
+ */
 const addTypeField = (data, type) => data.map(el => ({...el, type}));
 
 
+/**
+ * Returns 
+ * @param {Object} args (input) - input string (can be anything like 'e', 'err', 'zzz')
+ * @returns {Object} - returns list of data for different data types matching the input string
+ */
 const search = async ({ input }) => {
-    console.log('hahahaha', input);
     const genes = addTypeField(await getGenesBasedOnSymbol(input), 'gene');
 
     return (genes);
 };
+
 
 module.exports = {
     search,
