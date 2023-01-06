c036e47d25b75e76dc34bc4a625f34d70465181c
gangeshberi <gangeshberi@gmail.com>
2023-01-06 13:32:44 -0500

    minor change

--- client/src/Components/SummaryComponents/Datasets/Datasets.js
+++ client/src/Components/SummaryComponents/Datasets/Datasets.js
@@ -37,7 +37,7 @@ const renderComponent = (loading, error, columns, data) => {
   return (
     <>
       <DatasetIntersection />
-      <h2 style={{marginTop: '50px'}}>Datasets</h2>
+      <h2 style={{marginTop: '50px'}}> List of Datasets </h2>
       <Table columns={columns} data={data} />
     </>
   );
