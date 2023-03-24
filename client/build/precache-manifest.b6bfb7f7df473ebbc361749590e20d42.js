0b29dfd1b221d2127bb8d3268cad0837e8e486a9
gangeshberi <gangeshberi@gmail.com>
2023-03-24 12:00:51 -0400

    new build march 24th

---
+++ client/build/precache-manifest.b6bfb7f7df473ebbc361749590e20d42.js
@@ -0,0 +1,402 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db70b912483f4af8850011b9b1c8b866",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ffbd0648535876301b6",
+    "url": "/static/css/2.3f25759a.chunk.css"
+  },
+  {
+    "revision": "0ffbd0648535876301b6",
+    "url": "/static/js/2.c7c49dd8.chunk.js"
+  },
+  {
+    "revision": "b2e1071cde35bde2f7edf6bfa6a1580e",
+    "url": "/static/js/2.c7c49dd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60c405536ea9acb06124",
+    "url": "/static/js/main.05976ffb.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.05976ffb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  },
+  {
+    "revision": "3e94a194567e26cd3d85ca40b92baf6d",
+    "url": "/static/media/DatasetCellList.3e94a194.png"
+  },
+  {
+    "revision": "f5cfd76eaf54d43fc14284ed7f632e59",
+    "url": "/static/media/DatasetDrugList.f5cfd76e.png"
+  },
+  {
+    "revision": "f2501459ac6cfd64edd4e8a2ec63a7e7",
+    "url": "/static/media/bg.f2501459.webp"
+  },
+  {
+    "revision": "b58cf0bbb09323aab35d91c33174021d",
+    "url": "/static/media/biomarker_comp.b58cf0bb.png"
+  },
+  {
+    "revision": "586eb829f499ace5022a540cebf177a7",
+    "url": "/static/media/biomarker_forest.586eb829.png"
+  },
+  {
+    "revision": "3afec85f2db6a4aea4cf943fc9a3698a",
+    "url": "/static/media/biomarker_gene.3afec85f.png"
+  },
+  {
+    "revision": "0a590fd8b0065e9344d6997719c1047c",
+    "url": "/static/media/biomarker_manhattan.0a590fd8.png"
+  },
+  {
+    "revision": "3577410e7f5d401e5f282792801b7a5d",
+    "url": "/static/media/bone_annot.3577410e.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "0ed8eeb85c7eb0089ae249d333a73a9b",
+    "url": "/static/media/breast_pacli_plot 3.16.23 PM.0ed8eeb8.png"
+  },
+  {
+    "revision": "2d200d79913eb60b097a57e2e2fac542",
+    "url": "/static/media/breast_pacli_table.2d200d79.png"
+  },
+  {
+    "revision": "aabf91085906e68e56e8f4f09b039b0a",
+    "url": "/static/media/cell.aabf9108.webp"
+  },
+  {
+    "revision": "57e09421fe1de56e2d5a2fe67d70da1d",
+    "url": "/static/media/cellDrugSearch.57e09421.png"
+  },
+  {
+    "revision": "6c2c3956b8d595157efa2c5d5f164d06",
+    "url": "/static/media/cellIndBar.6c2c3956.png"
+  },
+  {
+    "revision": "775346f606f905c4745b9fae48c032ca",
+    "url": "/static/media/cellSearch.775346f6.png"
+  },
+  {
+    "revision": "688687eb4cf8f199d50b4dd3332de91a",
+    "url": "/static/media/cellSummaryList.688687eb.png"
+  },
+  {
+    "revision": "fe9ed429b8cc140bd85c832c67e87cdf",
+    "url": "/static/media/cellline_hela_aac.fe9ed429.png"
+  },
+  {
+    "revision": "9ceb4c9d3b37e2e28e0b2901de520d5f",
+    "url": "/static/media/cellline_hela_molprof.9ceb4c9d.png"
+  },
+  {
+    "revision": "78d9c8dfd45fc5b50460e2dfc025a2eb",
+    "url": "/static/media/cellline_hela_summary.78d9c8df.png"
+  },
+  {
+    "revision": "a91b8f4a0b70104aa520adb90ac7afcd",
+    "url": "/static/media/close.a91b8f4a.png"
+  },
+  {
+    "revision": "4b40234bdc1b6e2b01a3c0377c9cb10e",
+    "url": "/static/media/compound.4b40234b.webp"
+  },
+  {
+    "revision": "76eed327c81a1678e0d400cb6d09398a",
+    "url": "/static/media/dataset.76eed327.webp"
+  },
+  {
+    "revision": "df16d8e900a7d4f418f2d49a1401d480",
+    "url": "/static/media/datasetDType.df16d8e9.png"
+  },
+  {
+    "revision": "14c2617474cd436f2d316afdbfc0b0a1",
+    "url": "/static/media/datasetInd.14c26174.png"
+  },
+  {
+    "revision": "fd75cc36baecb76506a4f75ee7457fba",
+    "url": "/static/media/datasetIndBarA.fd75cc36.png"
+  },
+  {
+    "revision": "568d76e1118f26f30192c19ba2d75a63",
+    "url": "/static/media/datasetIndBarB.568d76e1.png"
+  },
+  {
+    "revision": "106c61d7d2cacd636bef86c11a053ef8",
+    "url": "/static/media/datasetList.106c61d7.png"
+  },
+  {
+    "revision": "17e9f99e8eac63e8f352e27f61b54492",
+    "url": "/static/media/datasetResource.17e9f99e.png"
+  },
+  {
+    "revision": "2862150502b11f4a256fdbfad3125bab",
+    "url": "/static/media/datasetSearch.28621505.png"
+  },
+  {
+    "revision": "a1d8e00ed5d62625a304e3f910a1dc5c",
+    "url": "/static/media/datasetUpsetPlot.a1d8e00e.png"
+  },
+  {
+    "revision": "2c62c1b73a01b953f5058af3e8d9487b",
+    "url": "/static/media/datasetsIntersection.2c62c1b7.png"
+  },
+  {
+    "revision": "f03394fe16587177a0697e196a73d4d8",
+    "url": "/static/media/desc-dosecurves.f03394fe.webp"
+  },
+  {
+    "revision": "01dd7df0841cee1571cdea9196f6ec03",
+    "url": "/static/media/download.01dd7df0.webp"
+  },
+  {
+    "revision": "1d9335b23d8f34fdc068db85606b4fdb",
+    "url": "/static/media/drugAACTissue.1d9335b2.png"
+  },
+  {
+    "revision": "17668aec662b911939e326f3fe19aa7c",
+    "url": "/static/media/drugIndCells.17668aec.png"
+  },
+  {
+    "revision": "546321aa594c03d71ea1301e5237777d",
+    "url": "/static/media/drugIndTissues.546321aa.png"
+  },
+  {
+    "revision": "f676bcea6924f12b63236cb7c2e41a3e",
+    "url": "/static/media/drugIndivBar.f676bcea.png"
+  },
+  {
+    "revision": "362d95c5319f143156ec06a52c48d80c",
+    "url": "/static/media/drugSearch.362d95c5.png"
+  },
+  {
+    "revision": "2c41350ea443525d7a3d4ef319e6750d",
+    "url": "/static/media/drugSummaryBar.2c41350e.png"
+  },
+  {
+    "revision": "9a04d7703f27a7bf471f116c29ad48ea",
+    "url": "/static/media/drugTissueSearch.9a04d770.png"
+  },
+  {
+    "revision": "cc926ab0d0f2b88fac7084f248f09fec",
+    "url": "/static/media/exmpBreast.cc926ab0.png"
+  },
+  {
+    "revision": "92f1995c12267809de251839a19c1234",
+    "url": "/static/media/exmpMCF-7.92f1995c.png"
+  },
+  {
+    "revision": "be5e6d77bbeb3c9f129c67d3f41b850b",
+    "url": "/static/media/exmpMCF-7Paclitaxel.be5e6d77.png"
+  },
+  {
+    "revision": "dc6e5aa8684418b2945b5d81586e2d13",
+    "url": "/static/media/exmpPaclitaxel.dc6e5aa8.png"
+  },
+  {
+    "revision": "7828b845c7cf15ccb731c141ecaeac58",
+    "url": "/static/media/experiment.7828b845.webp"
+  },
+  {
+    "revision": "8497a86ec7a0f3a3a64595e6867e32a7",
+    "url": "/static/media/experimentsBars.8497a86e.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ec09077fcc9a8b5a60c6209f9e09613",
+    "url": "/static/media/gene.0ec09077.webp"
+  },
+  {
+    "revision": "0a33670ffa0f06eb4c556f153741ede1",
+    "url": "/static/media/geneCompoundSearch.0a33670f.png"
+  },
+  {
+    "revision": "0f12ab67f77fa2293017f4e0e1248d26",
+    "url": "/static/media/geneCompoundTissueSearch.0f12ab67.png"
+  },
+  {
+    "revision": "5b5891274ac47155dcb8a647e764f84b",
+    "url": "/static/media/geneSummaryList.5b589127.png"
+  },
+  {
+    "revision": "cdc7e510a4dfc9467c66a92f5f1d06d5",
+    "url": "/static/media/geneSummaryPlot.cdc7e510.png"
+  },
+  {
+    "revision": "ea3a22eeaba1311bc61e7a0e4a249e55",
+    "url": "/static/media/genes_targets.ea3a22ee.png"
+  },
+  {
+    "revision": "328803204cb85c03e0b9bb0c7bbadb41",
+    "url": "/static/media/homeCellLines.32880320.png"
+  },
+  {
+    "revision": "d45cbd41dacaa7000c181d5017cbf516",
+    "url": "/static/media/homeCompounds.d45cbd41.png"
+  },
+  {
+    "revision": "1792cee3879a6bf18209f18547391577",
+    "url": "/static/media/homeDatasets.1792cee3.png"
+  },
+  {
+    "revision": "88f893c4659508611475083686528927",
+    "url": "/static/media/homeExperiments.88f893c4.png"
+  },
+  {
+    "revision": "0ea934a74e837d1a18d1775aa7847314",
+    "url": "/static/media/homeGenes.0ea934a7.png"
+  },
+  {
+    "revision": "d4a20294f505cab9277fa63b3971d485",
+    "url": "/static/media/homeNav.d4a20294.png"
+  },
+  {
+    "revision": "c491cb34320a22653fd7ad6b72469eb7",
+    "url": "/static/media/homeTissues.c491cb34.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "56c1959701ecce55c29acd58e674c276",
+    "url": "/static/media/magnif-glass.56c19597.png"
+  },
+  {
+    "revision": "3aecbf617ab2c5ed8436e78e32065278",
+    "url": "/static/media/mcf-7PacliSearch.3aecbf61.png"
+  },
+  {
+    "revision": "9067484b9751d4a6f4513ca78a1143be",
+    "url": "/static/media/mcf7_pacli_table.9067484b.png"
+  },
+  {
+    "revision": "f92c3ae699ca6c841014ef62fb09698a",
+    "url": "/static/media/mcf_pacli_plot.f92c3ae6.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "70e322a9d3a6e9a0742de559a09889ab",
+    "url": "/static/media/pacli_aac_celllines.70e322a9.png"
+  },
+  {
+    "revision": "9411065d830dec13cc0a56065afc7122",
+    "url": "/static/media/pacli_synonym.9411065d.png"
+  },
+  {
+    "revision": "ba884367cb9516a0ea6f7226c0a440de",
+    "url": "/static/media/pacli_target.ba884367.png"
+  },
+  {
+    "revision": "457b3463061257dcbd8ef5d2d67dcaa5",
+    "url": "/static/media/pharmacodb-logo-capsule.457b3463.png"
+  },
+  {
+    "revision": "cd4abf488d8af7c9195237154c952e3e",
+    "url": "/static/media/pharmacodb-logo-dark.cd4abf48.png"
+  },
+  {
+    "revision": "3d66b03945cc53f668460a2eb07acd80",
+    "url": "/static/media/pharmacodb-logo.3d66b039.png"
+  },
+  {
+    "revision": "787f525518dbe2f440c6276b825c4373",
+    "url": "/static/media/pieChart.787f5255.png"
+  },
+  {
+    "revision": "30cde8bc6c6331baf6bc358bffb71776",
+    "url": "/static/media/tissue.30cde8bc.webp"
+  },
+  {
+    "revision": "f7084b8d56f07646a46f88495f1f8a6d",
+    "url": "/static/media/tissueCellList.f7084b8d.png"
+  },
+  {
+    "revision": "76b6a514c0d3a5daf91a9a37c30b34fa",
+    "url": "/static/media/tissueDrugSummary.76b6a514.png"
+  },
+  {
+    "revision": "a4d00572c64b1fb26af5846f4ec5489a",
+    "url": "/static/media/tissueIndBar.a4d00572.png"
+  },
+  {
+    "revision": "fa3ff93f849a963b583c370a09169583",
+    "url": "/static/media/tissueList.fa3ff93f.png"
+  },
+  {
+    "revision": "61db3f9abff1e78949d07282539790b7",
+    "url": "/static/media/tissueSearch.61db3f9a.png"
+  },
+  {
+    "revision": "c25f339a30191f6740280e41b174b07a",
+    "url": "/static/media/tissuebreast_drugpacli.c25f339a.png"
+  },
+  {
+    "revision": "6760b715030d465ae4dff6f09408eded",
+    "url": "/static/media/x-circle-fill.6760b715.svg"
+  }
+]);
